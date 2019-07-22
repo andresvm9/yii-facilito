a5b50e5e3a5c40cb0863eaf7ce7a1f16d44a9133
Andrés Vargas Marín <andresvm9@gmail.com>
2019-07-22 05:16:41 -0600

    Cambiando index para ver si funciona en azure

--- facilito2/index.php
+++ facilito2/index.php
@@ -1,14 +1,60 @@
 <?php
+ include_once "vendor/autoload.php";
+ /*
+  * Create the body of the message (a plain-text and an HTML version).
+  * $text is your plain-text email
+  * $html is your html version of the email
+  * If the receiver is able to view html emails then only the html
+  * email will be displayed
+  */
+ 
+ $text = "Hi!\nHow are you?\n";
+ $html = "<html>
+       <head></head>
+       <body>
+           <p>Hi!<br>
+               How are you?<br>
+           </p>
+       </body>
+       </html>";
+ // This is your From email address
+ $from = array('andresvm9@gmail.com' => 'Andres Vargas');
+ // Email recipients
+ $to = array(
+       'andres_v_m@hotmail.com'=>'Andres2',
+       'avargas@vorealis.com'=>'Andres Vorealis'
+ );
+ // Email subject
+ $subject = 'Example PHP Email';
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+ // Login credentials
+ $username = 'andresvm9';
+ $password = 'Jesusteamo_9';
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+ // Setup Swift mailer parameters
+ $transport = new Swift_SmtpTransport('smtp.sendgrid.net', 587);
+ $transport->setUsername($username);
+ $transport->setPassword($password);
+ $swift = new Swift_Mailer($transport);
 
+ // Create a message (subject)
+ $message = new Swift_Message($subject);
 
-require_once($yii);
-Yii::createWebApplication($config)->run();
+ // attach the body of the email
+ $message->setFrom($from);
+ $message->setBody($html, 'text/html');
+ $message->setTo($to);
+ $message->addPart($text, 'text/plain');
+
+ // send message
+ if ($recipients = $swift->send($message, $failures))
+ {
+     // This will let us know how many users received this message
+     echo 'Message sent out to '.$recipients.' users';
+ }
+ // something went wrong =(
+ else
+ {
+     echo "Something went wrong - ";
+     print_r($failures);
+ }
