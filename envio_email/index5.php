aa79bc8e37d9b3cb94dbad0d625b0752e5dfe13c
Andres Vargas <andresvm9@gmail.com>
2019-07-23 11:56:54 -0600

    Se crea una clase envio_email

---
+++ envio_email/index5.php
@@ -0,0 +1,15 @@
+<?php 
+
+$From = "sender@test.com";
+$To = "<Your Mailaddress>";
+$SMTPServer = "smtp.sendgrid.net";
+$SMTPPort = "587";
+$Username = "azure_bcf0125d65eb6f663a0cef9b05dddc76@azure.com";
+$Password = "VoreTask?29";
+$subject = "Test5";
+$body = "Esta es solo una prueba";
+$smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
+$smtp.EnableSSL = $true;
+$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+$smtp.Send($From, $To, $subject, $body);
+?>
