a7c7b159dec08afa7ab546f4e48ae25d03ddd278
Andres Vargas <andresvm9@gmail.com>
2019-07-26 08:03:41 -0600

    Se agregan archivos de prueba de envio por diferentes metodos

---
+++ facilito2/index4.php
@@ -0,0 +1,22 @@
+<?php 
+include_once "vendor/autoload.php";
+
+// Create the Transport
+$transport = (new Swift_SmtpTransport('smtp.example.org', 587))
+  ->setUsername('azure_bcf0125d65eb6f663a0cef9b05dddc76@azure.com')
+  ->setPassword('VoreTask?29')
+;
+
+// Create the Mailer using your created Transport
+$mailer = new Swift_Mailer($transport);
+
+// Create a message
+$message = (new Swift_Message('Wonderful Subject'))
+  ->setFrom(['admin@taskblast.io' => 'Task Blast'])
+  ->setTo(['andres_v_m@hotmail.com', 'other@domain.org' => 'Heisenberg'])
+  ->setBody('This is just a test')
+  ;
+
+// Send the message
+$result = $mailer->send($message);
+ ?>
---
+++ facilito2/index5.php
@@ -0,0 +1,15 @@
+<?php 
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+require 'vendor/autoload.php';
+$sendgrid = new SendGrid("SENDGRID_APIKEY");
+$email    = new SendGrid\Email();
+
+$email->addTo("test@sendgrid.com")
+      ->setFrom("you@youremail.com")
+      ->setSubject("Sending with SendGrid is Fun")
+      ->setHtml("and easy to do anywhere, even with PHP");
+
+$sendgrid->send($email);
+
+ ?>
