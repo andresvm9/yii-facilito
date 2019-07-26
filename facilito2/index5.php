5f93cc2c0f925eaa11a00a072374555eb8553000
Andres Vargas <andresvm9@gmail.com>
2019-07-26 10:54:32 -0600

    Update index5.php

--- facilito2/index5.php
+++ facilito2/index5.php
@@ -3,12 +3,12 @@
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
 require 'vendor/autoload.php';
-$sendgrid = new SendGrid("SENDGRID_APIKEY");
+$sendgrid = new SendGrid("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");
 $email    = new SendGrid\Email();
 
 
-$email->addTo("avargas@vorealis.com")
-      ->setFrom("you@youremail.com")
+$email->addTo("andres_v_m@hotmail.com")
+      ->setFrom("avargas@vorealis.com")
       ->setSubject("Sending with SendGrid is Fun")
       ->setHtml("and easy to do anywhere, even with PHP");
 
